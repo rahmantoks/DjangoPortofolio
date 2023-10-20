db205f3d1c079a4909a7c841a9cf1493445a18e3
rahmantoks <rahmanto@outlook.com>
2023-10-20 15:06:18 +0000

    modified os variable

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangodb',
         'USER': 'djangouser',
-        'PASSWORD': os.environ["DJANGO_POSTGRESQL_PASSWORD"],
+        'PASSWORD': os.environ.get("DJANGO_POSTGRESQL_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '',
     }
