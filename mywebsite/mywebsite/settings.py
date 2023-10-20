462d0b769508c340198c6c075ba638bd73ab79e3
rahmantoks <rahmanto@outlook.com>
2023-10-20 14:27:37 +0000

    modified staicfiles

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -129,8 +129,3 @@ MEDIA_URL = 'images/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "staticfiles",
-]
