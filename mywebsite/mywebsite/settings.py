ec54611fb78009568650b9121f41b5bac573cf17
rahmantoks <rahmanto@outlook.com>
2023-10-20 14:49:19 +0000

    changed static setting

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -129,3 +129,5 @@ MEDIA_URL = 'images/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
