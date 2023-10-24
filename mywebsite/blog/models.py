59b9236efeaff0b9351e446747374731392c2b11
rahmanto <rahmanto@iecha.com>
2023-10-24 20:00:56 +0900

    added bare blog

---
+++ mywebsite/blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.user.get_username()
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    subtitle = models.CharField(max_length=20)
+    slug = models.SlugField()
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    overview = models.TextField()
+    timetamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    thumbnail = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    featured = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
