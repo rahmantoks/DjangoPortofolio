177f17e7a68102fcde3b9a14faf4168d65ebd555
rahmantoks <rahmanto@outlook.com>
2023-11-04 05:00:49 +0000

    switched markdown to summernote, added shadow to cards

--- mywebsite/blog/admin.py
+++ mywebsite/blog/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import Author, Category, Post
+from django_summernote.admin import SummernoteModelAdmin
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    summenote_fields = ('overview',)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
 
