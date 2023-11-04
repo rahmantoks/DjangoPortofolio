74bc93670c8835203f579c7995b2e3fd25623210
rahmantoks <rahmanto@outlook.com>
2023-11-04 13:34:13 +0000

    added datetime, language, tags to blog post

--- mywebsite/blog/admin.py
+++ mywebsite/blog/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Author, Category, Post
+from .models import Author, Category, Post, Tag, Language
 from django_summernote.admin import SummernoteModelAdmin
 
 class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    summenote_fields = ('overview',)
+    summernote_fields = ('overview','short')
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
-# Register your models here.
 admin.site.register(Author)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Tag)
+admin.site.register(Language)
--- mywebsite/blog/models.py
+++ mywebsite/blog/models.py
@@ -2,10 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 
-
 User = get_user_model()
 
-# Create your models here.
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -14,19 +12,37 @@ class Author(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=20)
-    subtitle = models.CharField(max_length=20)
+    explanation = models.TextField()
     slug = models.SlugField()
 
     def __str__(self):
         return self.title
-    
+
+class Tag(models.Model):
+    title = models.CharField(max_length=20)
+    explanation = models.TextField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+class Language(models.Model):
+    language_id = models.CharField(max_length=2)
+    language = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.language
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     overview = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    short = models.TextField()
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tag)
+    lang = models.ForeignKey(Language,on_delete=models.CASCADE)
     featured = models.BooleanField()
 
     def __str__(self):
